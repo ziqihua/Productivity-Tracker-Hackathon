8b7aab96254f12cab767d7de06e93466c7eb0276
HuiYu (Nollie) Chen <nolliechy@gmail.com>
2024-01-26 14:41:29 -0500

    user mode semi done

--- habit-tracker/src/App.js
+++ habit-tracker/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import LandingPage from './components/LandingPage/LandingPage';
 import HabitCategory from './components/HabitCategory/HabitCategory';
 import JobCategory from './components/HabitCategory/JobRelated';
+import TaskRecommendation from './components/TaskRecommendation/TaskRecommendation';
 
 function App() {
   return (
     <div className="App">
       {/* <LandingPage /> */}
       {/* <HabitCategory/> */}
-      <JobCategory />
+      {/* <JobCategory /> */}
+      <TaskRecommendation/>
     </div>
   );
 }
