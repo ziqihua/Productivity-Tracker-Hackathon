ddb336f2ddb575320bb3688cf49041023f16fc6b
HuiYu (Nollie) Chen <nolliechy@gmail.com>
2024-01-27 18:00:03 -0500

    set layout

--- habit-tracker/src/components/Community/CommunityBoard.js
+++ habit-tracker/src/components/Community/CommunityBoard.js
@@ -1,7 +1,25 @@
-import "../CSS/CommunityBoard.css"
+import "../CSS/CommunityBoard.css";
+import ActivityFeed from "../ActivityFeed/ActivityFeed";
+import AnnoucementBar from "./AnnoucementBar";
+import UserLists from "./UserLists";
 
 function CommunityBoard() {
+  return (
+    <div className="community-board-container">
+      <h2>Community</h2>
+      <div className="top-section">
+        <AnnoucementBar />
+      </div>
+      <div className="bottom-section">
+        <div className="activity-feed">
+          <ActivityFeed />
+        </div>
+        <div className="user-lists">
+          <UserLists />
+        </div>
+      </div>
+    </div>
+  );
 }
-
 
 export default CommunityBoard;
