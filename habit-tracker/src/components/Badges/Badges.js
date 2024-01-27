3bef8ebc74a4ffd77701821c38a7ac8ff6a3a525
Ziqi Hua <ziqihua@seas.upenn.edu>
2024-01-27 14:17:24 -0500

    Add Badges Images

--- habit-tracker/src/components/Badges/Badges.js
+++ habit-tracker/src/components/Badges/Badges.js
@@ -1,10 +1,10 @@
-import '../CSS/Badges.css'; // Import your CSS stylesheet
+import "../CSS/Badges.css"; // Import your CSS stylesheet
 
 function Badges() {
-    return (
-        <div className="badges">
-        <h1>Badges</h1>
-        </div>
-    );
-    }
+  return (
+    <div className="badges">
+      <h1>Badges Achievements</h1>
+    </div>
+  );
+}
 export default Badges;
