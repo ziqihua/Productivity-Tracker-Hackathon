f32fcc8ff912d417b421ba77c201df8890da2a54
HuiYu (Nollie) Chen <nolliechy@gmail.com>
2024-01-27 10:59:01 -0500

    create template

---
+++ habit-tracker/src/components/Community/ActivityFeeds.js
@@ -0,0 +1,11 @@
+import "../CSS/ActivityFeeds.css";
+
+function ActivityFeeds() {
+  return (
+    <div className="activity-feeds">
+      <h1>Activity Feeds</h1>
+    </div>
+  );
+}
+
+export default ActivityFeeds;
---
+++ habit-tracker/src/components/Community/UserLists.js
@@ -0,0 +1,11 @@
+import "../CSS/UserLists.css";
+
+function UserLists() {
+  return (
+    <div className="user-lists">
+      <h1>User Lists</h1>
+    </div>
+  );
+}
+
+export default UserLists;
