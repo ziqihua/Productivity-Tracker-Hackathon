9a03e527beccee0f550d4c1b96221ac601c42736
Ziqi Hua <ziqihua@seas.upenn.edu>
2024-01-27 23:40:59 -0500

    badges

--- habit-tracker/src/components/Badges/Badges.js
+++ habit-tracker/src/components/Badges/Badges.js
@@ -15,12 +15,29 @@ const Badge = ({
   return (
     <div className="badge">
       <p className="badge-title">{title}</p>
-      <img className="image1" src={image1} alt="Badge" />
-      <img className="image2" src={image2} alt="Badge-1w" />
-      <img className="image3" src={image3} alt="Badge-3w" />
-      <img className="image4" src={image4} alt="Badge-5w" />
-      <img className="image5" src={image5} alt="Badge-10w" />
-      <img className="image6" src={image6} alt="Badge-15w" />
+      <img className="image1" src={image1} alt="Badge-icon" />
+      <div className="badge-text-container">
+        <div className="badge-text">
+          <img className="image2" src={image2} alt="Badge-1w" />
+          1W
+        </div>
+        <div className="badge-text">
+          <img className="image3" src={image3} alt="Badge-3w" />
+          3W
+        </div>
+        <div className="badge-text">
+          <img className="image4" src={image4} alt="Badge-5w" />
+          5W
+        </div>
+        <div className="badge-text">
+          <img className="image5" src={image5} alt="Badge-10w" />
+          10W
+        </div>
+        <div className="badge-text">
+          <img className="image6" src={image6} alt="Badge-15w" />
+          15W
+        </div>
+      </div>
       <p className="badge-description">{description}</p>
     </div>
   );
@@ -37,29 +54,86 @@ function Badges() {
       image6: require("../../pics/job_hunting_undone.png"),
       title: "Job-related",
       description:
-        "Come on, you do want to land a decent job after graduation, right? Build your job hunting routine now!",
+        "6 weeks staying habitual! Stay positive and figure crossed :) ",
     },
     // Repeat for each badge...
+
+    {
+      image1: require("../../pics/Education pic.png"),
+      image2: require("../../pics/Education finished.png"),
+      image3: require("../../pics/Education finished.png"),
+      image4: require("../../pics/Education finished.png"),
+      image5: require("../../pics/Education undone.png"),
+      image6: require("../../pics/Education undone.png"),
+      title: "Education",
+      description:
+        "7 weeks staying habitual! Great job in keeping yourself from procrastinating.",
+    },
+
+    {
+      image1: require("../../pics/Health pic.png"),
+      image2: require("../../pics/Health finished.png"),
+      image3: require("../../pics/health undone.png"),
+      image4: require("../../pics/health undone.png"),
+      image5: require("../../pics/health undone.png"),
+      image6: require("../../pics/health undone.png"),
+      title: "Health",
+      description:
+        "1 week staying habitual! It’s never late to start nurturing lifelong friendships.",
+    },
+
+    {
+      image1: require("../../pics/hobby pic.png"),
+      image2: require("../../pics/hobby finished.png"),
+      image3: require("../../pics/hobby finished.png"),
+      image4: require("../../pics/hobbies undone.png"),
+      image5: require("../../pics/hobbies undone.png"),
+      image6: require("../../pics/hobbies undone.png"),
+      title: "Hobby",
+      description: "4 weeks staying habitual! Enrich your life everyday.",
+    },
+
+    {
+      image1: require("../../pics/Socializing pic.png"),
+      image2: require("../../pics/Social undone.png"),
+      image3: require("../../pics/Social undone.png"),
+      image4: require("../../pics/Social undone.png"),
+      image5: require("../../pics/Social undone.png"),
+      image6: require("../../pics/Social undone.png"),
+      title: "Social",
+      description:
+        "It’s never late to start nurturing lifelong friendships. Build your social routine today!",
+    },
+
+    {
+      image1: require("../../pics/Reflection pic.png"),
+      image2: require("../../pics/Reflection undone.png"),
+      image3: require("../../pics/Reflection undone.png"),
+      image4: require("../../pics/Reflection undone.png"),
+      image5: require("../../pics/Reflection undone.png"),
+      image6: require("../../pics/Reflection undone.png"),
+      title: "Reflection",
+      description:
+        "Do you know our daily lives are a reflection of what we believe in? Build your reflection routine today!",
+    },
   ];
 
   return (
     <div className="badges">
       <h2>Badges Achievements</h2>
-      <div className="badge-container">
-        {badgeData.map((badge, index) => (
-          <Badge
-            key={index}
-            title={badge.title}
-            image1={badge.image1}
-            image2={badge.image2}
-            image3={badge.image3}
-            image4={badge.image4}
-            image5={badge.image5}
-            image6={badge.image6}
-            description={badge.description}
-          />
-        ))}
-      </div>
+      {badgeData.map((badge, index) => (
+        <Badge
+          key={index}
+          title={badge.title}
+          image1={badge.image1}
+          image2={badge.image2}
+          image3={badge.image3}
+          image4={badge.image4}
+          image5={badge.image5}
+          image6={badge.image6}
+          description={badge.description}
+        />
+      ))}
     </div>
   );
 }
