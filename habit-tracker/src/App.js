c20e329ef277a1b8a2f7840660cf4d100c1d6b2b
Ziqi Hua <ziqihua@seas.upenn.edu>
2024-01-27 19:35:31 -0500

    badges

--- habit-tracker/src/App.js
+++ habit-tracker/src/App.js
@@ -2,7 +2,7 @@
 import "./App.css";
 import LoginPage from "./components/LoginPage/LoginPage";
 import Registration from "./components/Registration/Registration";
-import { React, useState } from "react";
+import { useState } from "react";
 import LandingPage from "./components/LandingPage/LandingPage";
 import HabitCategory from "./components/HabitCategory/HabitCategory";
 import JobCategory from "./components/HabitCategory/JobRelated";
@@ -14,6 +14,7 @@ import ActivityFeed from "./components/ActivityFeed/ActivityFeed";
 import Tracker from "./components/Tracker/Tracker";
 import Badges from "./components/Badges/Badges";
 import { Sidebar } from "react-pro-sidebar";
+import CommunityBoard from "./components/Community/CommunityBoard";
 
 function App() {
   let dicViewsNumbers = {};
@@ -22,8 +23,13 @@ function App() {
   dicViewsNumbers["TodoList"] = 2;
   dicViewsNumbers["Tracker"] = 3;
   dicViewsNumbers["Badges"] = 4;
+  dicViewsNumbers["LandingPage"] = 5;
+  dicViewsNumbers["HabitCategory"] = 6;
+  dicViewsNumbers["JobCategory"] = 7;
+  dicViewsNumbers["TaskRecommendation"] = 8;
+  dicViewsNumbers["CommunityBoard"] = 9;
 
-  const [curView, setCurView] = useState(dicViewsNumbers["Badges"]); // the state to decide which view to present
+  const [curView, setCurView] = useState(dicViewsNumbers["Registration"]); // the state to decide which view to present
   const [curUserId, setUserId] = useState(-1);
 
   let view;
@@ -56,24 +62,64 @@ function App() {
         updateUserId={setUserId}
       />
     );
+  } else if (curView === dicViewsNumbers["LandingPage"]) {
+    view = (
+      <LandingPage
+        updateCurView={setCurView}
+        userId={curUserId}
+        updateUserId={setUserId}
+      />
+    );
+  } else if (curView === dicViewsNumbers["HabitCategory"]) {
+    view = (
+      <HabitCategory
+        updateCurView={setCurView}
+        userId={curUserId}
+        updateUserId={setUserId}
+      />
+    );
+  } else if (curView === dicViewsNumbers["JobCategory"]) {
+    view = (
+      <JobCategory
+        updateCurView={setCurView}
+        userId={curUserId}
+        updateUserId={setUserId}
+      />
+    );
+  } else if (curView === dicViewsNumbers["TaskRecommendation"]) {
+    view = (
+      <TaskRecommendation
+        updateCurView={setCurView}
+        userId={curUserId}
+        updateUserId={setUserId}
+      />
+    );
+  } else if (curView === dicViewsNumbers["CommunityBoard"]) {
+    view = (
+      <CommunityBoard
+        updateCurView={setCurView}
+        userId={curUserId}
+        updateUserId={setUserId}
+      />
+    );
   }
 
+  const noSideBar = new Set([0, 1, 5, 6, 7, 8]);
+
   return (
-    <Badges updateCurView={setCurView} />
-    // <div>
-    //   {curView < 2 ? (
-    //     <>
-    //       {view}
-    //     </>
-    //   ) : (
-    //     <div style={{ display: 'flex', height: '100vh' }}>
-    //       <SidebarPanel updateCurView = {setCurView}/>
-    //       <div style={{ flex: 1, padding: '20px' }}>
-    //         {view}
-    //       </div>
-    //     </div>
-    //   )}
-    // </div>
+    // <>
+    //    <FormDialog />
+    // </>
+    <div>
+      {noSideBar.has(curView) ? (
+        <>{view}</>
+      ) : (
+        <div style={{ display: "flex", height: "100vh" }}>
+          <SidebarPanel updateCurView={setCurView} />
+          <div style={{ flex: 1, padding: "20px" }}>{view}</div>
+        </div>
+      )}
+    </div>
   );
 }
 
